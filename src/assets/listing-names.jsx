0870f3f5b0c383781c385b6adf4394f627ab006f
ShaQay3Q <shaghay3gh@gmail.com>
2024-07-11 22:04:00 +0200

    broken code. I hate js, I should have gone with ts

--- src/assets/listing-names.jsx
+++ src/assets/listing-names.jsx
@@ -28,7 +28,10 @@ export default function listingNames() {
   const names = ['Ali', 'Taghi', 'Naghi', 'Mojtaba', 'Asghar', 'Afsar', 'Ghanbar', 'Delbar'];
 
   const loggedIn = true;
-
+  const styles = {
+    color: 'green',
+    fontSize: '20px'
+  };
   return (
     <div>
       <div className="text-5xl">My App</div>
@@ -43,7 +46,8 @@ export default function listingNames() {
       </ul>
       <p style={{color: 'red'}}>{loggedIn ? <h2>you are logged in.</h2> : <h2>please log in!</h2>}</p>
       {/* if the loggedIn is false, then it is gonna show an empty string */}
-      <p>{loggedIn ? <h2>Hello Member</h2> : ''}</p>
+      {/*!!! STYLE gets an OBJECT */}
+      <p style={styles}>{loggedIn ? <h2>Hello Member</h2> : ''}</p>
       {/* if loggedIn is true, then it will execute the second part */}
       <p>{loggedIn && <h2>I said hello!</h2>}</p>
     </div>
