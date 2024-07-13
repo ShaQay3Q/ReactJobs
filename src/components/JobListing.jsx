879e73a9d0f2a41a38be17e8aeb2d85ad615a2fe
ShaQay3Q <shaghay3gh@gmail.com>
2024-07-13 20:30:40 +0200

    start using useState and onClick (the onClick one was interesting. It called a fucntion that called another function)

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -1,9 +1,17 @@
 import propTypes from "prop-types";
+import { useState } from "react";
+import { FaMapMarker } from "react-icons/fa";
 
 JobListing.propTypes = {
 	job: propTypes.object,
 };
 export default function JobListing({ job }) {
+	const [showFullDescription, setShowFullDescription] = useState(false);
+	let description = job.description;
+
+	if (!showFullDescription) {
+		description = description.substring(0, 90) + "...";
+	}
 	return (
 		<div>
 			<div
@@ -16,7 +24,14 @@ export default function JobListing({ job }) {
 						<h3 className='text-xl font-bold'>{job.title}</h3>
 					</div>
 
-					<div className='mb-5'>{job.description}</div>
+					<div className='mb-5'>{description}</div>
+
+					<button
+						onClick={() => setShowFullDescription((prevState) => !prevState)}
+						className='text-indigo-500 mb-5 hover:text-indigo-700'
+					>
+						{showFullDescription ? "Less" : "More"}
+					</button>
 
 					<h3 className='text-indigo-500 mb-2'>{job.salary} / Year</h3>
 
@@ -24,7 +39,7 @@ export default function JobListing({ job }) {
 
 					<div className='flex flex-col lg:flex-row justify-between mb-4'>
 						<div className='text-orange-700 mb-3'>
-							<i className='fa-solid fa-location-dot text-lg'></i>
+							<FaMapMarker className='inline text-lg mb-1 mr-1' />
 							{job.location}
 						</div>
 						<a
